c9d885be06036458e41ee34d3aa59d16efdff400
Datcu31 <datcu31@gmail.com>
2023-09-13 18:54:05 +0300

    update

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -6,13 +6,13 @@ function NavigationBar() {
             <nav>
                 <ul className="flex gap-8 items-center justify-center sm:gap-20 md:gap-28">
                     <li className="transition hover:-translate-y-1">
-                        <NavLink to="/" className={({ isActive }) => (isActive ? "border-b-2 border-black" : "")} end>Home</NavLink>
+                        <NavLink to="/my-portfolio" className={({ isActive }) => (isActive ? "border-b-2 border-black" : "")} end>Home</NavLink>
                     </li>
                     <li className="transition hover:-translate-y-1">
                         <NavLink to="projects" className={({ isActive }) => isActive ? "border-b-2 border-black" : ""}>Portfolio</NavLink>
                     </li>
                     <li className="transition hover:scale-110">
-                        <NavLink to="/" end className="text-3xl font-semibold tracking-widest">A|D</NavLink>
+                        <NavLink to="/my-portfolio" end className="text-3xl font-semibold tracking-widest">A|D</NavLink>
                     </li>
                     <li className="transition hover:-translate-y-1">
                         <NavLink to="contact" className={({ isActive }) => (isActive ? "border-b-2 border-black" : "")}>Contact</NavLink>
