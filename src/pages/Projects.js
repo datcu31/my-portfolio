f612e362c8f7e38bd3cf78255e4a32933d402c19
Datcu31 <datcu31@gmail.com>
2023-10-05 15:48:18 +0300

    Portfolio page update

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,20 +1,58 @@
+import React, { useState, useEffect } from "react";
 import ProjectContainer from "../components/ProjectContainer";
 import { info } from "../info/Info";
 
 function Projects() {
+    const [visibleProjects, setVisibleProjects] = useState(1); // Number of projects initially visible
+    const [scrolling, setScrolling] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (!scrolling) {
+                setScrolling(true);
+                // Determine if the user has scrolled to the bottom
+                if (
+                    window.innerHeight + window.scrollY >=
+                    document.body.offsetHeight - 40
+                ) {
+                    // Load more projects when the user is near the bottom
+                    setVisibleProjects((prevVisibleProjects) =>
+                        Math.min(prevVisibleProjects + 1, info.portfolio.length)
+                    );
+                }
+                setScrolling(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [scrolling]);
+
     return (
         <div className="flex flex-col md:mt-10">
             <div className="animate-moveInDown">
-                <p className="flex place-content-center text-sm lg:text-base font-light">View my work</p>
-                <h1 className="flex text-4xl lg:text-5xl place-content-center mb-8 font-extrabold">Projects</h1>
+                <p className="flex place-content-center text-sm lg:text-base font-light">
+                    View my work
+                </p>
+                <h1 className="flex text-4xl lg:text-5xl place-content-center mb-8 font-extrabold">
+                    Projects
+                </h1>
             </div>
-            <ul>{info.portfolio.map((project, index) => (
-                <li key={index}>
-                    <ProjectContainer image={project.image} live={project.live} source={project.source} title={project.title} />
-                </li>
-            ))}
+            <ul>
+                {info.portfolio.slice(0, visibleProjects).map((project, index) => (
+                    <li key={index}>
+                        <ProjectContainer
+                            image={project.image}
+                            live={project.live}
+                            source={project.source}
+                            title={project.title}
+                        />
+                    </li>
+                ))}
             </ul>
-
         </div>
     );
 }
